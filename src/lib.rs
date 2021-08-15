9baa745e499118f2945a10c0cd51b81345b236c9
d3npa <d3npa@pm.me>
2021-08-15 19:13:57 -0400

    clean a little

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,17 @@ use tokio::net::TcpListener;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
+
+pub const MAX_HEADER_LENGTH: usize = 1026;
+
+pub mod response_codes {
+    pub const INPUT: usize = 10;
+    pub const INPUT_SENSITIVE: usize = 11;
+    pub const SUCCESS: usize = 20;
+    pub const REDIRECT_TEMPORARY: usize = 30;
+    pub const REDIRECT_PERMANENT: usize = 31;
+}
+
 const GEMINI_LOCALHOST: &str = "127.0.0.1:1965";
 const X509_PROFILE: &[u8] = include_bytes!("../identity.pfx");
 
@@ -20,7 +31,7 @@ pub async fn create_tcp_listener() -> Result<TcpListener> {
 }
 
 pub fn create_response(
-    code: i32, meta: Option<&str>, body: Option<&str>) -> Vec<u8>
+    code: usize, meta: Option<&str>, body: Option<&str>) -> Vec<u8>
 {
     let meta = meta.unwrap_or("");
     let mut response = format!("{} {}\r\n", code, meta);
